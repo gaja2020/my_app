0cc0d3f7ff0faabd7b340bed391251a6932c6806
gaja2020 <cjshms@gmail.com>
2018-07-12 14:20:05 +0900

    five

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-  res.send('Hello World');
-});
-
+var path = require('path')
+app.use(express.static(path.join(__dirname + '/public')));
+console.log(__dirname)  ;
 app.listen(3000, function(){
   console.log('Server On!');
 });
