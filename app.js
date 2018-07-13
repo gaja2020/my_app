7a3d216d63345e74a8de50662e1004fbd6f47c27
gaja2020 <cjshms@gmail.com>
2018-07-13 09:00:44 +0900

    six

--- app.js
+++ app.js
@@ -1,8 +1,68 @@
 var express = require('express');
 var app = express();
 var path = require('path')
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_DB);
+var db=mongoose.connection;
+
+db.once("open",function(){
+  console.log("DB Connected!!");
+});
+
+db.on("error",function(err){
+  console.log("DB Error!!", err);
+});
+
+var dataschema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+
+var Data = mongoose.model('data',dataschema);
+
+Data.findOne({name:"myData"},function(err,data){
+  console.log("Data is ", data);
+  if(err) return console.log("Data Error : ", err);
+  if(!data){
+    Data.create({name:"myData", count:0}, function(err,data){
+      if(err) return console.log("Data Error : ", err);
+      console.log("Counter initialized : ", data);
+    });
+  }
+});
+
+
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname + '/public')));
-console.log(__dirname)  ;
+
+app.get('/',function(req,res) {
+  Data.findOne({name:"myData"},function(err,data){
+    if(err) return console.log("Data Error : ", err);
+    data.count++;
+    data.save(function(err){
+        if(err) return console.log("Data Error : ", err);
+        console.log(data);
+        res.render('my_first_ejs',data);
+    });
+  });
+});
+
+app.get('/reset',function(req,res) {
+  data.count = 0;
+  res.render('my_first_ejs',data);
+});
+
+app.get('/set/count',function(req,res) {
+  if(req.query.count) data.count=req.query.count;
+    res.render('my_first_ejs',data);
+});
+
+app.get('/set/:num',function(req,res) {
+    data.count=req.params.num;
+    res.render('my_first_ejs',data);
+});
+
 app.listen(3000, function(){
   console.log('Server On!');
 });
